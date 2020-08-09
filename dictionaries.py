1aace941e7ec240c9d91732ab3ef62108d2d7646
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-09 18:15:34 +0000

    Add package

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,11 @@
+#functions
+def print_package(**kwargs):
+    for key,value in kwargs.items():
+        print("{}:{}".format(key,value))
+
+
 course = {'name':'Piter', 'surname':'Molenda', 'skill':'Beginner'}
+
 print(course['name'])
 print(course.keys())
 course['name'] = 'Marek'
@@ -7,3 +14,17 @@ course['language'] = 'English'
 print(course)
 del(course['language'])
 print(course)
+print("")
+# wypisze tylko i wylacznie keys
+for item in course:
+    print(item)
+print("")
+#wypisze wartosci
+for item in course:
+    print(course[item])
+print("")
+for item in course.items():
+    key,value = item
+    print("{} : {}".format(key,value))
+
+print_package(one = 12,two = 32,three = 43,four = 'okej')
