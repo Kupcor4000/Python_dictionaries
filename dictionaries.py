5218c4422f0a7aa92af920b4785b8bb0da3e6a8d
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-09 17:57:29 +0000

    Add some methods

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,9 @@
-course = ['Ashley', 'Introducing Dictionaries', 'Beginner']
+course = {'name':'Piter', 'surname':'Molenda', 'skill':'Beginner'}
+print(course['name'])
+print(course.keys())
+course['name'] = 'Marek'
+print(course)
+course['language'] = 'English'
+print(course)
+del(course['language'])
+print(course)
